4609dcfd8c2f4233dd2a759f09f7c2f582c28974
Pablo <soypab@gmail.com>
2018-11-10 19:04:25 +0100

    flume update

--- installLocal/properties.sh
+++ installLocal/properties.sh
@@ -1,3 +1,3 @@
-export scriptHome=/user/raj_ops/installProcessDocker/schoolsProcess
+export scriptHome=/home/vagrant/installProcess/schoolsProcess
 export inputPath=$scriptHome/data/202311-0-colegios-publicos.csv
 export ouPath=$scriptHome/data/colegios-publicos.csv
